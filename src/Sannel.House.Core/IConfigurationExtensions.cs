de1c699e76c92b4e5d09193b058e2fbb0d19be9e
Adam Holt <holtsoftware@outlook.com>
2023-06-02 20:12:08 -0600

    Inital House.Core push

---
+++ src/Sannel.House.Core/IConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Sannel.House;
+public static class IConfigurationExtensions
+{
+
+	/// <summary>
+	/// Gets the configured group ID.
+	/// </summary>
+	/// <param name="configuration">The configuration.</param>
+	/// <param name="groupId">The group identifier.</param>
+	/// <returns>The group ID, or an empty string if not found.</returns>
+	public static string GetGroupById(this IConfiguration configuration, string groupId)
+	{
+		ArgumentNullException.ThrowIfNull(configuration);
+		ArgumentException.ThrowIfNullOrEmpty(groupId);
+
+		return configuration[$"Roles:{groupId}"] ?? string.Empty;
+	}
+}
---
+++ src/Sannel.House.Core/Roles.cs
@@ -0,0 +1,15 @@
+﻿namespace Sannel.House;
+
+public static class Roles
+{
+	public const string ADMIN = "House.Admin";
+
+	public static class Sprinklers
+	{
+		public const string SCHEDULE_READ = "House.Sprinklers.Schedule.Read";
+		public const string SCHEDULE_WRITE = "House.Sprinklers.Schedule.Write";
+		public const string ZONE_READ = "House.Sprinklers.Zone.Read";
+		public const string ZONE_WRITE = "House.Sprinklers.Zone.Write";
+
+	}
+}
